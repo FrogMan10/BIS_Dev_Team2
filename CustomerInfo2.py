62fe1201100f2d021e5ddbb546da8113bdb3fc3d
chucanh2910 <78053359+chucanh2910@users.noreply.github.com>
2021-04-26 21:11:22 -0500

    Update CustomerInfo2.py

--- CustomerInfo2.py
+++ CustomerInfo2.py
@@ -1,12 +1,61 @@
 from tkinter import * # Import tkinter
 import random   #import random for Order Numbers
-
+def datavalid():
+    if lastname.get() == '':
+        Alert1.set('This is the required field. Please enter your last name.')
+    elif lastname.get() != '':
+        Alert1.set('')
+    if firstname.get() == '':
+        Alert2.set('This is the required field. Please enter your first name.')
+    elif firstname.get() != '':
+        Alert2.set('')
+    if cellnumber.get() == '':
+        Alert3.set('This is a required field. Please enter your cellphone number.')
+    cell = list(cellnumber.get())
+    for t in cell:
+        try:
+            number = int(t)
+            pass
+        except ValueError:
+            Alert3.set('The cellphone number could not contain string.')
+        if 0 < number < 9: 
+            if len(cell) != 0:
+                Alert3.set('')
+    if email.get() == '':
+        Alert4.set('This is the required field. Please enter your email')
+    elif '@' not in email.get():
+        Alert4.set('Please enter the valid email adress. Sample: name@gmail.com')
+    elif '@' in email.get():
+        Alert4.set('')
+    if numdogs.get() == 'Up to 3 dogs':
+        Alert5.set('Please choose your number of dogs')
+    if numdogs.get() != 'Up to 3 dogs':
+        Alert5.set('')
+    if dogname1.get() == '':
+        Alert6.set('This is the required field. Please enter your dog\'s name.')
+    elif dogname1.get() != '':
+        Alert6.set('')
+    if dogbreed1.get() == '':
+        Alert7.set('This is the required field. Please enter your dog\'s breed.')
+    elif dogbreed1.get() != '':
+        Alert7.set('')
+    if SpayedNeutered.get() == 'Are all of your dog spayed or neutured?':
+        Alert8.set('Please choose one of the option in drop-down menu list')
+    elif SpayedNeutered.get() != 'Are all of your dog spayed or neutured?':
+       Alert8.set('') 
+    if shots.get() == 'Are all of your dog recieved shots?':
+        Alert9.set('Please choose one of the option in drop-down menu list')
+    else:
+        Alert9.set('')
+        Alert10.set('Customer information comfirmed. Please go to next page')
+    
+        
 CusInfo=Tk()
-CusInfo.geometry('450x600')
+CusInfo.geometry('450x650')
 CusInfo.title("Customer Information") 
 CusInfo.configure(background='orange')
 pic = PhotoImage(file=
-"C:\\Mutts.png")
+"D:\\Mutts.png")
 pic = pic.zoom(20,5) 
 pic = pic.subsample(46,15)
 Label(CusInfo, image = pic, background='orange').grid(row=0, columnspan = 3, sticky = W+E)
@@ -19,21 +68,20 @@ Label(CusInfo, text = 'Cell Number*',background='orange').grid(row=6, column=1, 
 Label(CusInfo, text = '', background='orange').grid(row=7)
 Label(CusInfo, text = 'Email*',background='orange').grid(row=8, column=1, sticky=W)
 Label(CusInfo, text = '', background='orange').grid(row=9)
-Label(CusInfo, text = 'Number of Dogs(Up to 3)',background='orange').grid(row=10, column=1, sticky=W )
+Label(CusInfo, text = 'Number of Dogs',background='orange').grid(row=10, column=1, sticky=W )
 Label(CusInfo, text = '', background='orange').grid(row=11)
-Label(CusInfo, text = '', background='orange').grid(row=12)
-Label(CusInfo, text = 'Dog 1 Name *',background='orange').grid(row=13, column=1, sticky=W )
+Label(CusInfo, text = 'Dog 1 Name *',background='orange').grid(row=12, column=1, sticky=W )
 Label(CusInfo, text = 'Dog 2 Name',background='orange').grid(row=14, column=1, sticky=W )
 Label(CusInfo, text = 'Dog 3 Name',background='orange').grid(row=15, column=1, sticky=W )
 Label(CusInfo, text = '', background='orange').grid(row=16)
 Label(CusInfo, text = 'Dog 1 Breed *',background='orange').grid(row=17, column=1, sticky=W )
-Label(CusInfo, text = 'Dog 2 Breed', background='orange').grid(row=18, column=1, sticky=W )
-Label(CusInfo, text = 'Dog 3 Breed', background='orange').grid(row=19, column=1, sticky=W )
-Label(CusInfo, text = '', background='orange').grid(row=20)
-Label(CusInfo, text = 'Spayed/Neutered*', background='orange').grid(row=21, column = 1, sticky = W)
-Label(CusInfo, text = '', background='orange').grid(row=22)
-Label(CusInfo, text = 'Shots Received*', background='orange').grid(row=23, column = 1, sticky = W)
-Label(CusInfo, text = '', background='orange').grid(row=24)
+Label(CusInfo, text = 'Dog 2 Breed', background='orange').grid(row=19, column=1, sticky=W )
+Label(CusInfo, text = 'Dog 3 Breed', background='orange').grid(row=20, column=1, sticky=W )
+Label(CusInfo, text = '', background='orange').grid(row=21)
+Label(CusInfo, text = 'Spayed/Neutered*', background='orange').grid(row=22, column = 1, sticky = W)
+Label(CusInfo, text = '', background='orange').grid(row=23)
+Label(CusInfo, text = 'Shots Received*', background='orange').grid(row=24, column = 1, sticky = W)
+Label(CusInfo, text = '', background='orange').grid(row=25)
 option = StringVar()
 
 
@@ -50,10 +98,14 @@ email = StringVar()
 Entry(CusInfo, textvariable = email, justify = LEFT, background='white').grid(row=8, column = 2, sticky = W)
 
 numdogs = StringVar()
-Entry(CusInfo, textvariable = numdogs, justify = LEFT, background='white').grid(row=10, column = 2,sticky = W)
+options = {'1', '2', '3'}
+numdogs.set('Up to 3 dogs')
+available_options = OptionMenu(CusInfo, numdogs, *options)
+available_options.configure(width = 13, bg = 'white', highlightthickness = 1)
+available_options.grid(row = 10, column = 2, sticky = W)
 
 dogname1 = StringVar()
-Entry(CusInfo, textvariable = dogname1, justify = LEFT, background='white').grid(row=13, column = 2, sticky = W)
+Entry(CusInfo, textvariable = dogname1, justify = LEFT, background='white').grid(row=12, column = 2, sticky = W)
 
 dogname2 = StringVar()
 Entry(CusInfo, textvariable = dogname2, justify = LEFT, background='white').grid(row=14, column = 2, sticky = W)
@@ -62,22 +114,60 @@ dogname3 = StringVar()
 Entry(CusInfo, textvariable = dogname3, justify = LEFT, background='white').grid(row=15, column = 2, sticky = W)
 
 dogbreed1 = StringVar()
-Entry(CusInfo, textvariable = dogname3, justify = LEFT, background='white').grid(row=17, column = 2, sticky = W)
+Entry(CusInfo, textvariable = dogbreed1, justify = LEFT, background='white').grid(row=17, column = 2, sticky = W)
 
 dogbreed2 = StringVar()
-Entry(CusInfo, textvariable = dogname3, justify = LEFT, background='white').grid(row=18, column = 2, sticky = W)
+Entry(CusInfo, textvariable = dogbreed2, justify = LEFT, background='white').grid(row=19, column = 2, sticky = W)
 
 dogbreed3 = StringVar()
-Entry(CusInfo, textvariable = dogname3, justify = LEFT, background='white').grid(row=19, column = 2, sticky = W)
+Entry(CusInfo, textvariable = dogbreed3, justify = LEFT, background='white').grid(row=20, column = 2, sticky = W)
 
-spayNeuter = StringVar()
-Entry(CusInfo, textvariable = spayNeuter, justify = LEFT, background = 'white').grid(row = 21, column = 2, sticky = W)
+SpayedNeutered = StringVar()
+options2 = {'Yes', 'No'}
+SpayedNeutered.set('Are all of your dog spayed or neutured?')
+available_options2 = OptionMenu(CusInfo, SpayedNeutered, *options2)
+available_options2.configure(width = 40, bg = 'white', highlightthickness = 1)
+available_options2.grid(row = 22, column = 2, sticky = W)
 
 shots = StringVar()
-Entry(CusInfo, textvariable = shots, justify = LEFT, background = 'white').grid(row = 23, column = 2, sticky = W)
+options3 = {'Yes', 'No'}
+shots.set('Are all of your dog recieved shots?')
+available_options3 = OptionMenu(CusInfo, shots, *options3)
+available_options3.configure(width = 40, bg = 'white', highlightthickness = 1)
+available_options3.grid(row = 24, column = 2, sticky = W)
+
+Alert1 = StringVar()
+Label(CusInfo, textvariable = Alert1, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 3, column = 2, sticky = W)
 
 
-Button(CusInfo, text = 'Confirm submission', justify = LEFT, bg = 'white'). grid(row = 25, column = 2, columnspan = 2, sticky = W)
-Button(CusInfo, text = 'Previous', justify = LEFT, bg = 'white'). grid(row = 25, column = 1, sticky = W)
+Alert2 = StringVar()
+Label(CusInfo, textvariable = Alert2, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 5, column = 2, sticky = W)
 
+Alert3 = StringVar()
+Label(CusInfo, textvariable = Alert3, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 7, column = 2, sticky = W)
+
+Alert4 = StringVar()
+Label(CusInfo, textvariable = Alert4, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 9, column = 2, sticky = W)
+
+Alert5 = StringVar()
+Label(CusInfo, textvariable = Alert5, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 11, column = 2, sticky = W)
+
+Alert6 = StringVar()
+Label(CusInfo, textvariable = Alert6, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 13, column = 2, sticky = W)
+
+Alert7 = StringVar()
+Label(CusInfo, textvariable = Alert7, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 18, column = 2, sticky = W)
+
+Alert8 = StringVar()
+Label(CusInfo, textvariable = Alert8, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 23, column = 2, sticky = W)
+
+Alert9 = StringVar()
+Label(CusInfo, textvariable = Alert9, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 25, column = 2, sticky = W)
+
+Alert10 = StringVar()
+Label(CusInfo, textvariable = Alert10, justify = LEFT, bg = 'orange', fg = 'red'). grid(row = 27, column = 2, sticky = W)
+
+Button(CusInfo, text = 'Confirm submission', justify = LEFT, bg = 'white', command = datavalid). grid(row = 26, column = 2, columnspan = 2, sticky = W)
+Button(CusInfo, text = 'Previous', justify = LEFT, bg = 'white'). grid(row = 26, column = 1, sticky = W)
+Button(CusInfo, text = 'Next', justify = LEFT, bg = 'white').grid(row = 26, column = 3, sticky = W)
 CusInfo.mainloop()
